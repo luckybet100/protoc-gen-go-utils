094e8f6e53a007dc990543dc9b60d8853a25ba92
Алексей Кулдошин <kuldoshin.alexey@gmail.com>
2021-08-28 01:35:19 +0300

    add must load/store

--- fdb.go
+++ fdb.go
@@ -25,6 +25,21 @@ func genLoadFromFDB(gFile *protogen.GeneratedFile, config genFDBConfig) {
 	gFile.P()
 }
 
+func genMustLoadFromFDB(gFile *protogen.GeneratedFile, config genFDBConfig) {
+	gFile.P("func MustLoad", config.structName, "FDB(tr fdb.ReadTransaction, key subspace.Subspace) *", config.structName, " {")
+	gFile.P("value := tr.Get(key).MustGet()")
+	gFile.P("if value == nil {")
+	gFile.P("return nil")
+	gFile.P("}")
+	gFile.P(config.objectName, ":=", "Acquire", config.structName, "()")
+	gFile.P("if err := proto.Unmarshal(value,", config.objectName, "); err != nil {")
+	gFile.P("panic(err)")
+	gFile.P("}")
+	gFile.P("return ", config.objectName, "")
+	gFile.P("}")
+	gFile.P()
+}
+
 func genStoreFDB(gFile *protogen.GeneratedFile, config genFDBConfig) {
 	gFile.P("func Store", config.structName, "FDB(tr fdb.Transaction, key subspace.Subspace, ", config.objectName, " *", config.structName, ") error {")
 	gFile.P("value, err := proto.Marshal(", config.objectName, ")")
@@ -33,6 +48,17 @@ func genStoreFDB(gFile *protogen.GeneratedFile, config genFDBConfig) {
 	gFile.P("}")
 	gFile.P("tr.Set(key, value)")
 	gFile.P("return nil")
+	gFile.P("}")
+	gFile.P()
+}
+
+func genMustStoreFDB(gFile *protogen.GeneratedFile, config genFDBConfig) {
+	gFile.P("func MustStore", config.structName, "FDB(tr fdb.Transaction, key subspace.Subspace, ", config.objectName, " *", config.structName, ") {")
+	gFile.P("value, err := proto.Marshal(", config.objectName, ")")
+	gFile.P("if err != nil {")
+	gFile.P("panic(err)")
+	gFile.P("}")
+	gFile.P("tr.Set(key, value)")
 	gFile.P("}")
 	gFile.P()
 }
@@ -55,5 +81,7 @@ func GenFdbMethods(gFile *protogen.GeneratedFile, message *protogen.Message) {
 		"github.com/apple/foundationdb/bindings/go/src/fdb/subspace",
 	})
 	genLoadFromFDB(gFile, config)
+	genMustLoadFromFDB(gFile, config)
 	genStoreFDB(gFile, config)
+	genMustStoreFDB(gFile, config)
 }
