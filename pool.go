0bd5049db324d9317db7e0235f8c3b0e1af33b6e
Алексей Кулдошин <kuldoshin.alexey@gmail.com>
2021-08-28 02:14:05 +0300

    remove release

--- pool.go
+++ pool.go
@@ -21,16 +21,12 @@ func genSyncPool(gFile *protogen.GeneratedFile, config genPoolConfig) {
 
 func genAcquire(gFile *protogen.GeneratedFile, config genPoolConfig) {
 	gFile.P("func Acquire", config.structName, "() *", config.structName, "{")
+	gFile.P(config.objectName, ":=", config.poolName, ".Get()", ".(*", config.structName, ")")
+	gFile.P("runtime.SetFinalizer(", config.objectName, ", func (x interface{}) {")
+	gFile.P("runtime.SetFinalizer(x, nil)")
+	gFile.P(config.poolName, ".Put(x)")
+	gFile.P("})")
 	gFile.P("return ", config.poolName, ".Get().(*", config.structName, ")")
-	gFile.P("}")
-	gFile.P()
-}
-
-func genRelease(gFile *protogen.GeneratedFile, config genPoolConfig) {
-	gFile.P("func Release", config.structName, "(", config.objectName, " *", config.structName, ") {")
-	gFile.P("if ", config.objectName, "!= nil {")
-	gFile.P(config.poolName, ".Put(", config.objectName, ")")
-	gFile.P("}")
 	gFile.P("}")
 	gFile.P()
 }
@@ -45,7 +41,10 @@ func GeneratePool(gFile *protogen.GeneratedFile, message *protogen.Message) {
 		"sync",
 		"sync",
 	})
+	gFile.QualifiedGoIdent(protogen.GoIdent{
+		"runtime",
+		"runtime",
+	})
 	genSyncPool(gFile, config)
 	genAcquire(gFile, config)
-	genRelease(gFile, config)
 }
