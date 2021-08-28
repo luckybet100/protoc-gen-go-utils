764b3421a036bd9b7bd2a4fc90180729d06043e8
Алексей Кулдошин <kuldoshin.alexey@gmail.com>
2021-08-28 17:37:15 +0300

    add release and resey

--- pool.go
+++ pool.go
@@ -22,11 +22,21 @@ func genSyncPool(gFile *protogen.GeneratedFile, config genPoolConfig) {
 func genAcquire(gFile *protogen.GeneratedFile, config genPoolConfig) {
 	gFile.P("func Acquire", config.structName, "() *", config.structName, "{")
 	gFile.P(config.objectName, ":=", config.poolName, ".Get()", ".(*", config.structName, ")")
-	gFile.P("runtime.SetFinalizer(", config.objectName, ", func (x interface{}) {")
-	gFile.P("runtime.SetFinalizer(x, nil)")
-	gFile.P(config.poolName, ".Put(x)")
+	gFile.P("runtime.SetFinalizer(", config.objectName, ", func (", config.objectName, " *", config.structName, ") {")
+	gFile.P("Release", config.structName, "(", config.objectName, ")")
 	gFile.P("})")
 	gFile.P("return ", config.poolName, ".Get().(*", config.structName, ")")
+	gFile.P("}")
+	gFile.P()
+}
+
+func genRelease(gFile *protogen.GeneratedFile, config genPoolConfig) {
+	gFile.P("func Release", config.structName, "(", config.objectName, " *", config.structName, ") {")
+	gFile.P("if ", config.objectName, " != nil {")
+	gFile.P("runtime.SetFinalizer(", config.objectName, ", nil)")
+	gFile.P(config.objectName, ".Reset()")
+	gFile.P(config.poolName, ".Put(", config.objectName, ")")
+	gFile.P("}")
 	gFile.P("}")
 	gFile.P()
 }
@@ -47,4 +57,5 @@ func GeneratePool(gFile *protogen.GeneratedFile, message *protogen.Message) {
 	})
 	genSyncPool(gFile, config)
 	genAcquire(gFile, config)
+	genRelease(gFile, config)
 }
